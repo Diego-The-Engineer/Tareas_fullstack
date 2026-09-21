eea7420fd9c5589669365e7a537fb5214f660bed
Diego <diegoaimargdlr@gmail.com>
2026-09-21 15:33:37 -0600

    Seccion 3 done!

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -11,47 +11,47 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-morgan.token('post-data', (request, response) => {
-    if(request.method === 'POST'){
-        return JSON.stringify(request.body)
-    }
-    return ' '
+morgan.token('post-data', (request) => {
+  if(request.method === 'POST'){
+    return JSON.stringify(request.body)
+  }
+  return ' '
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
 
 app.get('/api/persons', (request, response) => {
-    Person.find({})
+  Person.find({})
     .then(persons => {
-        response.json(persons)
+      response.json(persons)
     })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-        if(person) {
-            response.json(person)
-        } else {    
-            response.status(404).end()
-        }
-    })
+  Person.findById(request.params.id).then(person => {
+    if(person) {
+      response.json(person)
+    } else {    
+      response.status(404).end()
+    }
+  })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const data = request.body
+  const data = request.body
 
-    Person.findByIdAndUpdate(
-        request.params.id,
-        { number: data.number },
-        { new: true, runValidators: true }
-    )
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { number: data.number },
+    { new: true, runValidators: true }
+  )
     .then(res => {
-        if(res){
-            response.json(res)
-        }else{
-            response.status(404).end()
-        }
+      if(res){
+        response.json(res)
+      }else{
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
@@ -59,38 +59,38 @@ app.put('/api/persons/:id', (request, response, next) => {
 
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-    .then(res => {
-        response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if(!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const person = new Person ({
-        name: body.name,
-        number: body.number
+  if(!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing'
     })
+  }
+
+  const person = new Person ({
+    name: body.name,
+    number: body.number
+  })
     
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
     .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    Person.countDocuments({}).then(count => {
-        const fecha = new Date()
-        response.send(`<p> Phonebook has info for ${count} people </p> <p> ${fecha} </p>`)
-    })
+  Person.countDocuments({}).then(count => {
+    const fecha = new Date()
+    response.send(`<p> Phonebook has info for ${count} people </p> <p> ${fecha} </p>`)
+  })
 })
 
 
