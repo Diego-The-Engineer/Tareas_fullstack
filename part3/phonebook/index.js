06170e1b512a6a068f2220aaf178d3523d7f4587
Diego <diegoaimargdlr@gmail.com>
2026-09-21 12:23:19 -0600

    Agregando conectividad a mongo pt6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import 'dotenv/config'
-import Person from './models/person'
+import Person from './models/person.js'
 const PORT = process.env.PORT
 const app = express()
 app.use(express.json())
